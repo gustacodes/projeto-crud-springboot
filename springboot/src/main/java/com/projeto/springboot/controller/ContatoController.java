040b5696d6ddba45979f6dbe2df005633988d620
Gustavo Alencar <gustavorique@live.com>
2023-03-15 02:22:02 -0300

    finalizado

--- springboot/src/main/java/com/projeto/springboot/controller/ContatoController.java
+++ springboot/src/main/java/com/projeto/springboot/controller/ContatoController.java
@@ -3,8 +3,13 @@ package com.projeto.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +26,15 @@ public class ContatoController {
     
     @GetMapping
     public List<Contato> listar() {
-
         return repository.findAll();
+    }
+
+    @GetMapping("/listarporid/{id}")
+    public ResponseEntity<Contato> listarPorId(@PathVariable long id) {
+
+        Contato contato = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Contato"));
+
+                return ResponseEntity.ok(contato);            
 
     }
 
@@ -31,6 +43,32 @@ public class ContatoController {
        return repository.save(contato);
     }
 
-    
+    @PutMapping("/atualizarcontato/{id}")
+    public ResponseEntity<Contato> alterar(@PathVariable long id, @RequestBody Contato contato) {
+
+        Contato usuario = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Contato não existe: " + id));
+
+        usuario.setNome(contato.getNome());
+        usuario.setEmail(contato.getEmail());
+        usuario.setTelefone(contato.getTelefone());
+        usuario.setUrlAvatar(contato.getUrlAvatar());
+        
+        repository.save(usuario);
+        
+        return ResponseEntity.ok(usuario);
+
+     }
+
+    @DeleteMapping("/deletarcontato/{id}")
+    public String deletar(@PathVariable("id") long id) {
+
+       if(id > 0){
+            repository.deleteById(id);
+           return "Removido com sucesso";
+        } else {
+           return "Contato não encontrado"; 
+        }
+        
+     }
 
 }
